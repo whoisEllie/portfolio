d397c8d9461041c3d51669d14b4b3ff1c67d9609
whoisEllie <elliekelemen@protonmail.com>
2024-03-27 21:00:05 +0000

    Added homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="header">
+  <div>
+    <h2>
+      Hi! I'm Ellie.
+    </h2>
+    <h2>
+      I'm a consultant in software and games with almost a decade of experience
+    </h2>
+    <h2>
+      Very 90s, eh? ;D
+    </h2>
+  </div>
+  <div>
+    This is a detailed description of what i'm all about!
+  </div>
+</div>
+
+<style>
+  .header {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(2, 50%);
+  } 
+</style>
