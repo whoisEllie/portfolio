a6d06e5860417fa6d2034db8b9b7a040022b529a
whoisEllie <elliekelemen@protonmail.com>
2025-01-22 23:38:47 +0000

    Added carousel and additional formatting for slug

---
+++ src/lib/components/carousel.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+	import '@splidejs/svelte-splide/css';
+
+	let { images } = $props();
+</script>
+
+<Splide
+	options={{
+		type: 'loop',
+		width: 840,
+		autoWidth: true,
+		gap: '2rem',
+		padding: '15%',
+		autoHeight: false,
+		height: '40svh',
+		heightRatio: 0.5
+	}}
+>
+	{#each images as image}
+		<SplideSlide>
+			<img class="cover" src={image} />
+		</SplideSlide>
+	{/each}
+</Splide>
+
+<style>
+	img {
+		height: 40svh;
+		aspect-ratio: 16/9;
+		object-fit: cover;
+	}
+</style>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import type { PageProps } from '../$types';
-
 	let { data }: any = $props();
 	let Content = data.content;
 </script>
 
+<a href="/">Back</a>
 <div class="about-wrapper">
 	<article>
 		<Content />
@@ -17,6 +16,7 @@
 		flex-direction: column;
 		width: min(100% - 15px, 840px);
 		margin-inline: auto;
+		margin-top: 15svh;
 		justify-self: center;
 		align-items: center;
 	}
