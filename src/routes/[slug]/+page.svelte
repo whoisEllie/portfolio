8c9cf6a1d54711508c49a1b44614eda21db3a3ac
whoisEllie <elliekelemen@protonmail.com>
2025-01-23 00:11:26 +0000

    Polish up back button & slug page

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -3,7 +3,7 @@
 	let Content = data.content;
 </script>
 
-<a href="/">Back</a>
+<a class="back" href="/">Back</a>
 <div class="about-wrapper">
 	<article>
 		<Content />
@@ -16,8 +16,20 @@
 		flex-direction: column;
 		width: min(100% - 15px, 840px);
 		margin-inline: auto;
-		margin-top: 15svh;
+		margin-top: 10svh;
+		margin-bottom: 15svh;
 		justify-self: center;
 		align-items: center;
+	}
+
+	.back {
+		position: absolute;
+		top: 10px;
+		left: 10px;
+		text-decoration: none;
+		color: black;
+		padding: 5px 10px;
+		border-radius: 10px;
+		background-color: #e0e0e0;
 	}
 </style>
